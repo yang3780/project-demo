ce0eef74da938f08c0ac37495b214a708c0baded
yang3780 <3325451087@qq.com>
2026-04-07 07:57:55 +0800

    Remove deprecated MongoDB options

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const client = new MongoClient(uri);
 
 let db;
 
